05b0519d9c89c1c4a330bca3e0e43b340b673105
Michael Dunn <mdunn@alocus.com>
2016-09-22 13:31:33 -0400

    Add alamofire framwework

---
+++ Pods/Target Support Files/Pods-pokedexgo/Pods-pokedexgo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokedexgo : NSObject
+@end
+@implementation PodsDummy_Pods_pokedexgo
+@end
---
+++ Pods/Target Support Files/Pods-pokedexgo/Pods-pokedexgo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_pokedexgoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pokedexgoVersionString[];
+
