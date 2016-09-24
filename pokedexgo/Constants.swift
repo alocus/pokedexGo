be8ef0719f8ef4dd958a5edb9d30a470d9576fbd
Michael Dunn <mdunn@alocus.com>
2016-09-24 00:20:28 -0400

    Add pokemon details via pokeapi.co

---
+++ pokedexgo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedexgo
+//
+//  Created by Michael Dunn on 2016-09-22.
+//  Copyright © 2016 Michael Dunn. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON_V1 = "/api/v1/pokemon/"
+
+typealias DownloadCompleted = () -> ()
--- pokedexgo/PokemonDetailVC.swift
+++ pokedexgo/PokemonDetailVC.swift
@@ -28,8 +28,43 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
         
+        let img = UIImage(named: "\(pokemon.id)")
+        
+        imageView.image = img
+        currState.image = img
+        idLabel.text = "\(pokemon.id)"
+        typeLabel.text = pokemon.type
+        
+        
+        pokemon.downloadPokemonDetail(){
+                self.updateUI()
+        }
+        
+    }
+    
+    func updateUI(){
+     
+        attackLabel.text = pokemon.attack
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        typeLabel.text = pokemon.type
+        descriptionLabel.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            nextEvolutionLabel.text = "No Evolutions"
+            evolvedState.isHidden = true
+            
+        } else {
+            
+            evolvedState.isHidden = false
+            evolvedState.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            nextEvolutionLabel.text = str
+        }
+
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
